85aedca11a1a5183ce5812cef5d9df5b529af287
yanhackcode15 <iamyanhu@gmail.com>
2022-07-04 13:44:09 -0700

    updated my card content

--- src/components/main.js
+++ src/components/main.js
@@ -7,11 +7,11 @@ export default function Main() {
             <ul>
                 <li className="main-about">
                     <h1 className="about-title" >About</h1>
-                    <p className="about-text">I'm a nerd, love to build and automate tasks.</p>
+                    <p className="about-text">I'm an entrepruer. I love turning ideas into reality. My experiences include a good mix of working in the start-up world, running my own business, real estate design and development, and most recently fully commited to do web development. All my past experiences has led me to revist web dev in 2022, in a renewed, focused and rebooted spirit. I love every minute of it.</p>
                 </li>
                 <li className="main-interest">
                     <h1 className="interests-title">Interest</h1>
-                    <p className="interests-text">Travel around the world. Open busineses. Meet people and get to know different culture.</p>
+                    <p className="interests-text">Spicy food, writing code, automation, travel, friends, designing and building homes and parenting my two wonderful children, and many more...</p>
                 </li>
             </ul>
         </main>
